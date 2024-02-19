9f2d240a56e89e787a537b1487f854991b3b0308
Berkay <berkayp7@gmail.com>
2024-02-19 15:40:39 +0300

    vercel analytics eklendi.

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '@/components/navbar'
 import OverlayMenu from '@/components/navbar/overlayMenu'
+import { inject } from '@vercel/analytics'
 
 function RootLayout() {
+    inject()
+
     return (
         <>
             <Navbar />
