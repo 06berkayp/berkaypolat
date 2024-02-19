b4277c16efff099adef54c63e4055789b6882efd
Berkay <berkayp7@gmail.com>
2024-02-19 15:43:52 +0300

    vercel analytics düzenlemesi.

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,11 +1,8 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '@/components/navbar'
 import OverlayMenu from '@/components/navbar/overlayMenu'
-import { inject } from '@vercel/analytics'
 
 function RootLayout() {
-    inject()
-
     return (
         <>
             <Navbar />
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { RouterProvider } from 'react-router-dom'
 import routes from '@/routes'
 import { Provider as ReduxProvider } from 'react-redux'
 import store from '@/store'
+import { inject } from '@vercel/analytics'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ReduxProvider store={store}>
+        inject()
         <RouterProvider router={routes} />
     </ReduxProvider>
 )
